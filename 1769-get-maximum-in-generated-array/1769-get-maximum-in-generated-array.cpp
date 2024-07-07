0ba89a1f7f227b00334e2c4aef076a53e2518569
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2024-07-07 08:12:15 +0530

    Time: 0 ms (100%), Space: 7.6 MB (18.55%) - LeetHub

--- 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.cpp
@@ -12,7 +12,7 @@ public:
         nums[0] = 0;
         nums[1] = 1;
         
-        for(int i=1; i<(int)(n+1)/2; i++)
+        for(int i=1; 2 * i + 1 <= n; i++)
         {
             nums[2 * i] = nums[i];
             nums[2 * i + 1] = nums[i] + nums[i + 1];
