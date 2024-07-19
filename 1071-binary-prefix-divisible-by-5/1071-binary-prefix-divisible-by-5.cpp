a71a746508e1a8d0573a05bc069d9608fde1d74f
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2024-07-19 20:54:19 +0530

    Time: 8 ms (55.88%), Space: 16.6 MB (7.69%) - LeetHub

--- 1071-binary-prefix-divisible-by-5/1071-binary-prefix-divisible-by-5.cpp
+++ 1071-binary-prefix-divisible-by-5/1071-binary-prefix-divisible-by-5.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    std::vector<bool> prefixesDivBy5(std::vector<int>& nums) {
-        std::vector<bool> ans;
-        int currentNum = 0;  // Initialize once outside the loop
+    vector<bool> prefixesDivBy5(std::vector<int>& nums) 
+    {
+        vector<bool> ans;
+        int currentNum = 0; 
 
-        for (int i = 0; i < nums.size(); ++i) {
-            // Left shift currentNum by 1 bit and add the current digit
+        for (int i = 0; i < nums.size(); ++i) 
+        {    
             currentNum = (currentNum * 2 + nums[i]) % 5;
-            // Check if the current number is divisible by 5
+    
             ans.push_back(currentNum == 0);
         }
-
         return ans;
     }
 };
