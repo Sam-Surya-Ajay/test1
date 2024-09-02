a0c045b8198ed17583ef4f52741663babbfb7f37
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2024-09-02 14:30:40 +0530

    Time: 17 ms (35.42%), Space: 36.1 MB (13.85%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution 
+{
+public:
+    int finalPositionOfSnake(int n, vector<string>& commands) 
+    {
+        int ans=0;
+
+        vector<vector<int>> grid(n, vector<int>(n,0));
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                grid[i][j]=(i*n)+j;
+            }
+        }  
+
+        int i=0, j=0; 
+        for(int a=0; a<commands.size(); a++)
+        {
+            if(commands[a]=="UP")
+            {
+                i--;
+                ans=grid[i][j];
+            }
+            else if(commands[a]=="DOWN")
+            {
+                i++;
+                ans=grid[i][j];
+            }
+            else if(commands[a]=="LEFT")
+            {
+                j--;
+                ans=grid[i][j];
+            }
+            else if(commands[a]=="RIGHT")
+            {
+                j++;
+                ans=grid[i][j];
+            }
+        }
+        return ans;  
+    }
+};
