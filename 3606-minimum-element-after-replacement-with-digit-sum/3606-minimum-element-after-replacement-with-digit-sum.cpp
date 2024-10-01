43e46258ac83777577742185e495b6b2a347519a
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2024-10-01 19:30:06 +0530

    Time: 3 ms (90.66%), Space: 27.2 MB (42.8%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
@@ -6,12 +6,12 @@ public:
         int mini=INT_MAX;
         for(int i=0; i<nums.size(); i++)
         {
-            int temp=0, a=nums[i], b=nums[i]%10;
+            int temp=0, a=nums[i], b;
             while(a>0)
             {
-                temp+=b;
-                a/=10;
                 b=a%10;
+                temp+=b;
+                a/=10;  
             }
             nums[i]=temp;
             if(nums[i]<mini)
