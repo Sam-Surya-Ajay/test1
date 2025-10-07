8eaf24ca3a6c17737dde4a97c366a02e4eb602de
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2025-10-07 22:36:38 +0530

    Time: 0 ms (100%), Space: 8.3 MB (16.08%) - LeetHub

---
+++ 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    char shift(char a, int b)
+    {
+        return a+b;
+    }
+    string replaceDigits(string s) 
+    {
+        for(int i=1; i<s.length(); i+=2)
+        {
+            s[i]=shift(s[i-1], (s[i]-'0'));
+        }
+        return s;
+    }
+};
