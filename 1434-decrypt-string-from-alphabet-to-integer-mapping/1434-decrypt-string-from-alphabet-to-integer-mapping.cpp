92f985941e53e37b53ec7d2034f91e0b3ad81e34
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2025-10-26 15:03:31 +0530

    Time: 3 ms (9.24%), Space: 9.5 MB (9.87%) - LeetHub

---
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    string freqAlphabets(string s) 
+    {
+        string ans="";
+
+        for(int i=s.length()-1; i>=0; i--)
+        {
+            if(s[i]!='#')
+            {
+                int temp=(s[i]-'1');
+                char c='a'+temp;
+                ans=c + ans;
+            }
+            else
+            {
+                int temp=((s[i-2]-'0')*10+(s[i-1]-'0'))-1;
+                char c='a'+temp;
+                ans=c + ans;
+                i-=2;
+            }
+        }
+        return ans;
+    }
+};
