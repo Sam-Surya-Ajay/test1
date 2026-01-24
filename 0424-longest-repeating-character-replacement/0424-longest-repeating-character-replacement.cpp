e7c1fa8c1e0eedbbbf8f67af7f3e261830716873
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2026-01-24 11:16:19 +0530

    Time: 7 ms (45.95%), Space: 11.3 MB (5.34%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,42 @@
+class Solution 
+{
+public:
+    int characterReplacement(string s, int k) 
+    {
+        int ans=0, l=0;
+        
+        unordered_map<int, int> m;
+        for(int r=0; r<s.length(); r++)
+        {
+            m[s[r]]++;
+        
+            int maxchar=0;
+            for(auto it:m)
+            {
+                if(it.second>maxchar)
+                {
+                    maxchar=it.second;
+                }
+            }
+            
+            while((r-l+1)-maxchar > k)
+            {
+                m[s[l]]--;
+                if(m[s[l]]==0)
+                   m.erase(s[l]);
+                l++;
+
+                for(auto it:m)
+                {
+                   if(it.second>maxchar)
+                   {
+                       maxchar=it.second;
+                   }
+                }
+            }
+            ans=max(ans, r-l+1);
+        }
+
+        return ans;
+    }
+};
