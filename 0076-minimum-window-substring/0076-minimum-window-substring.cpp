f8ca6e613441da3cee18fdae27aafd6762f5447a
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2026-01-25 13:01:40 +0530

    Time: 238 ms (5.04%), Space: 372.3 MB (5.03%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,52 @@
+class Solution 
+{
+public:
+    string minWindow(string s, string t) 
+    {
+        string ans="";
+        int len=INT_MAX;
+
+        unordered_map<char, int> required, formed;
+        for(int i=0; i<t.length(); i++)
+        {
+            required[t[i]]++;
+        }
+        
+        int l=0, scount=0;
+        for(int r=0; r<s.length(); r++)
+        {
+            if(required.count(s[r]))
+            {
+                formed[s[r]]++;
+                if(formed[s[r]]==required[s[r]])
+                {
+                    scount++;
+                }
+            }
+            if(required.size()==scount)
+            {
+                while(l<=r)
+                {
+                    if(!required.count(s[l]))
+                    {
+                        l++;
+                    }
+                    else if(formed[s[l]]>required[s[l]])
+                    {
+                        formed[s[l]]--;
+                        l++;
+                    }
+                    else
+                        break;
+                }
+                if((s.substr(l,r-l+1)).length()<len)
+                { 
+                    ans=s.substr(l,r-l+1);
+                    len=ans.length();
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
