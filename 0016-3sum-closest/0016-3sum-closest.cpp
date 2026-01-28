0803067cdb796e842923568c1b153d71710f8b29
Sam-Surya-Ajay <153307499+Sam-Surya-Ajay@users.noreply.github.com>
2026-01-28 16:23:37 +0530

    Time: 282 ms (6.46%), Space: 14.1 MB (29.39%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,38 @@
+class Solution 
+{
+public:
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
+        int ans=0, diff=INT_MAX;
+
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<nums.size()-2; i++)
+        {
+            int l=i+1, r=nums.size()-1;
+            while(l<r)
+            {
+                if(abs((nums[i]+nums[l]+nums[r])-target) < diff)
+                {
+                    cout<<"HI "<<i<<" "<<l<<" "<<r<<'\n';
+                    ans=nums[i]+nums[l]+nums[r];
+                    diff=abs((nums[i]+nums[l]+nums[r])-target);
+                    if(ans < target)
+                        l++;
+                    else
+                        r--;
+                }
+                else if(nums[i]+nums[l]+nums[r] < target)
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
